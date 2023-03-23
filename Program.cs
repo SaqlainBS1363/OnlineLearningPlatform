33eaecf456bfdb4ea1edc49c05c232fb94239bb9
SaqlainBS1363 <anwarul.habib@brainstation-23.com>
2023-03-23 16:44:33 +0600

    Fixes user and admin panel options

--- Program.cs
+++ Program.cs
@@ -2,22 +2,22 @@
 {
     public class Program
     {
-        public static void Main(string[] args) 
+        public static void Main(string[] args)
         {
             var userList = new List<User>();
             var courseList = new List<Courses>();
 
-            Console.WriteLine("-- Online Learning Platform --");
-            Console.WriteLine("\n\n");
+            Console.WriteLine("---- Online Learning Platform ----");
+            Console.WriteLine("\n");
 
             while (true)
             {
-                Console.WriteLine("Login as 1. Admin, 2. User or 3. Exit\n");
+                Console.WriteLine("\nLogin as 1. Admin, 2. User or 3. Exit\n");
                 int loginChoice = Convert.ToInt32(Console.ReadLine());
 
                 if (loginChoice == 1)
                 {
-                    Console.Write("Enter AdminID: ");
+                    Console.Write("Enter Admin ID: ");
                     string adminId = Console.ReadLine();
 
                     Console.Write("Enter Password: ");
@@ -29,7 +29,7 @@
 
                         while (true)
                         {
-                            Console.WriteLine("Choose options: \n1. Check users \n2.Add course \n3. Log out.\n");
+                            Console.WriteLine("Choose options: \n1. Check users \n2. Add course \n3. Log out.\n");
 
                             int adminChoice = Convert.ToInt32(Console.ReadLine());
 
@@ -38,10 +38,11 @@
                                 Console.WriteLine("Currently registered users: \n");
                                 foreach (var user in userList)
                                 {
-                                    Console.Write("User ID: " + user.getuserId());
-                                    Console.Write("User Name: " + user.getuserName());
-                                    Console.Write("User Email: " + user.getemail());
+                                    Console.Write("User ID: " + user.getuserId() + " | "
+                                    + "Name: " + user.getuserName() + " | "
+                                    + "Email: " + user.getemail() + "\n");
                                 }
+                                Console.WriteLine("\n----------\n");
                             }
                             else if(adminChoice == 2)
                             {
@@ -72,12 +73,15 @@
 
                                 Console.WriteLine("New course added!\n");
                             }
-                            else if( adminChoice == 3)
+                            else if(adminChoice == 3)
                             {
                                 Console.WriteLine("Logging out of Admin Panel\n");
                                 break;
                             }
-                            else { Console.WriteLine("Wrong choice!"); }
+                            else 
+                            { 
+                                Console.WriteLine("Wrong choice!"); 
+                            }
                         }
                     }
                     else
@@ -89,34 +93,32 @@
                 {
                     while (true)
                     {
-                        Console.WriteLine("Enter choice: \n1.Sign up  \n2.Log in\n 3.Exit\n");
+                        Console.WriteLine("Enter choice: \n1. Sign up  \n2. Log in \n3. Exit \n");
 
                         int userchoice = Convert.ToInt32(Console.ReadLine());
 
                         if (userchoice == 1)
                         {
-                            Console.WriteLine("User sign up - \n");
+                            Console.WriteLine("\nUser sign up - \n");
 
-                            Console.WriteLine("Enter Name: ");
+                            Console.Write("Enter Name: ");
                             string uName = Console.ReadLine();
 
-                            Console.WriteLine("Enter Email: ");
+                            Console.Write("Enter Email: ");
                             string uEmail = Console.ReadLine();
 
-                            Console.WriteLine("Enter Password: ");
+                            Console.Write("Enter Password: ");
                             string uPassword = Console.ReadLine();
 
                             User newUser = new User();
                             newUser.setuserName(uName);
                             newUser.setemail(uEmail);
                             newUser.setpassword(uPassword);
-
                             Random rnd = new Random();
-
                             newUser.setuserId(uEmail + rnd.Next(1000).ToString());
 
                             userList.Add(newUser);
-                            Console.WriteLine("New user added!\n");
+                            Console.WriteLine("\n\nNew user added!\n\n");
                         }
                         else if (userchoice == 2)
                         {
@@ -142,63 +144,72 @@
                             {
                                 Console.WriteLine("\n\nWelcome " + user1.getuserName() + "\n\n");
 
-                                Console.WriteLine("Choose options: \n1. Check courses \n2.Buy course \n3. Log out.\n");
-
-                                int userChoice = Convert.ToInt32(Console.ReadLine());
-
-                                if (userChoice == 1)
+                                while (true)
                                 {
-                                    Console.WriteLine("Available courses are: ");
+                                    Console.WriteLine("Choose options: \n1. Check courses \n2. Buy course \n3. Log out.\n");
 
-                                    foreach (var crs in courseList)
+                                    int userChoice = Convert.ToInt32(Console.ReadLine());
+
+                                    if (userChoice == 1)
                                     {
-                                        Console.Write("Course ID: " + crs.getcourseID());
-                                        Console.WriteLine("Course Name: " + crs.getcourseName());
-                                        Console.WriteLine("Course Topic: " + crs.getcourseTopic());
-                                        Console.WriteLine("Course Price: " + crs.getcoursePrice());
-                                    }
-                                }
-                                else if (gotUser == 2)
-                                {
-                                    Console.Write("Enter Course ID: ");
-                                    string cId = Console.ReadLine();
+                                        Console.WriteLine("Available courses are: \n\n");
 
-                                    int foundCourse = 0;
-
-                                    foreach (var crs in courseList)
-                                    {
-                                        if (crs.getcourseID() == cId)
+                                        foreach (var crs in courseList)
                                         {
-                                            foundCourse = 1;
-                                            break;
+                                            Console.WriteLine("Course ID: " + crs.getcourseID() + " | "
+                                            + "Course Name: " + crs.getcourseName() + " | "
+                                            + "Course Topic: " + crs.getcourseTopic() + " | "
+                                            + "Course Price: " + crs.getcoursePrice() + "\n");
                                         }
+                                        Console.WriteLine("\n----------\n");
                                     }
+                                    else if (userChoice == 2)
+                                    {
+                                        Console.Write("\nEnter Course ID: ");
+                                        string cId = Console.ReadLine();
 
-                                    if(foundCourse == 1)
-                                        Console.WriteLine("Course Bought!\n");
+                                        int foundCourse = 0;
+
+                                        foreach (var crs in courseList)
+                                        {
+                                            if (crs.getcourseID() == cId)
+                                            {
+                                                foundCourse = 1;
+                                                break;
+                                            }
+                                        }
+
+                                        if (foundCourse == 1)
+                                            Console.WriteLine("Course Bought!\n");
+                                        else
+                                            Console.WriteLine("Course not found!\n");
+                                    }
+                                    else if (userChoice == 3)
+                                    {
+                                        Console.WriteLine("You are being logged out!\n");
+                                        break;
+                                    }
                                     else
-                                        Console.WriteLine("Course not found!\n");
-                                    break;
+                                    {
+                                        Console.WriteLine("Invalid entry!\n");
+                                    }
                                 }
-                                else if(gotUser == 3)
-                                {
-                                    Console.WriteLine("You are being logged out!\n");
-                                    break;
-                                }
-                                else
-                                {
-                                    Console.WriteLine("Invalid entry!\n");
-                                }                                
                             }
                             else
                             {
-                                Console.WriteLine("Invalid!\n");
+                                Console.WriteLine("No user found with these credentials!\n");
                             }
+                        }
+                        else if(userchoice == 3)
+                        {
+                            Console.WriteLine("User program exit!\n");
+                            break;
                         }
                     }
                 }
                 else if (loginChoice == 3)
                 {
+                    Console.WriteLine("Program exits\n");
                     break;
                 }
                 else
